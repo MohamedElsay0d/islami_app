91a70c9477a8d4a84aefdc8b1848ebaffd0fcd19
MohamedElsay0d <me827794@gmail.com>
2024-12-18 14:35:39 +0200

    import assets

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,19 +8,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     bottomNavigationBar: BottomNavigationBar(
-       items: const [
-         BottomNavigationBarItem(
-           icon: Icon(Icons.home),
-           activeIcon: Icon(Icons.home_outlined),
-           label: 'Home',
-         ),
-         BottomNavigationBarItem(
-           icon: Icon(Icons.settings),
-           label: 'Settings',
-         ),
-       ],
-     ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/images/icons/opened_book.svg'),
+            activeIcon: SvgPicture.asset(
+              'assets/images/icons/opened_book.svg',
+              colorFilter: const ColorFilter.mode(Colors.red, BlendMode.srcIn),
+            ),
+            label: 'Home',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
